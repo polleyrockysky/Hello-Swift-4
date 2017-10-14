722e84166a5731e754c3c5a11dfccbb0a1949358
polly <polleyrockysky@gmail.com>
2017-10-14 11:52:11 +0700

    Des UI ok

---
+++ Hello Swift4/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Hello Swift4
+//
+//  Created by USER on 10/14/17.
+//  Copyright © 2017 Polly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Explicit การประกาศตัวแปร แบบกำหนดค่า
+    var nameString: String = ""
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    } //Main Function สิ่งแรกที่จะทำงาน
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+} //Main Class
+
+
+
